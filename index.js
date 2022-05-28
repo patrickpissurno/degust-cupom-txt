fc6a0ea70de0e426450e5715062fc83233533f7f
Patrick Pissurno <contato@patrickpissurno.com.br>
2022-05-28 20:47:04 -0300

    Bugfix

--- index.js
+++ index.js
@@ -36,70 +36,79 @@ module.exports = function(txt){
         throw new CorruptedDataError('Arquivo corrompido.');
     }
 
-    let _tmp = lines[0].substr(40, 8);
+    const linha_0 = lines.find(x => x.startsWith('0'));
+    const linha_1 = lines.find(x => x.startsWith('1'));
+    const linha_2 = lines.find(x => x.startsWith('2'));
+    const linha_5 = lines.find(x => x.startsWith('5'));
+
+    let _tmp = linha_0.substr(40, 8);
     const data = _tmp.substr(0, 4) + '-' + _tmp.substr(4, 2) + '-' + _tmp.substr(6, 2);
 
-    _tmp = lines[1].substr(1, 8);
+    _tmp = linha_1.substr(1, 8);
     const data_caixa = _tmp.substr(0, 4) + '-' + _tmp.substr(4, 2) + '-' + _tmp.substr(6, 2);
     
-    _tmp = lines[0].substr(48, 6);
+    _tmp = linha_0.substr(48, 6);
     const hora = _tmp.substr(0, 2) + ':' + _tmp.substr(2, 2) + ':' + _tmp.substr(4, 2);
 
-    const loja_cnpj = lines[0].substr(26, 14);
+    const loja_cnpj = linha_0.substr(26, 14);
 
-    const tipo_venda = parseInt(lines[1].substr(20, 1));
-    const numero_abertura = parseInt(lines[1].substr(34, 2));
-    const controle_interno = parseInt(lines[1].substr(9, 10));
-    const controle_especifico = parseInt(lines[1].substr(21, 10));
-    const codigo_pdv = parseInt(lines[1].substr(31, 3));
+    const tipo_venda = parseInt(linha_1.substr(20, 1));
+    const numero_abertura = parseInt(linha_1.substr(34, 2));
+    const controle_interno = parseInt(linha_1.substr(9, 10));
+    const controle_especifico = parseInt(linha_1.substr(21, 10));
+    const codigo_pdv = parseInt(linha_1.substr(31, 3));
 
-    const somatorio_valor_produtos = parseFixedPoint(lines[1].substr(197, 12), 2);
-    const ind_acres_desc = lines[1].substr(209, 1).trim() || null;
-    const valor_acres_desc = parseFixedPoint(lines[1].substr(210, 12), 2);
+    const somatorio_valor_produtos = parseFixedPoint(linha_1.substr(197, 12), 2);
+    const ind_acres_desc = linha_1.substr(209, 1).trim() || null;
+    const valor_acres_desc = parseFixedPoint(linha_1.substr(210, 12), 2);
 
     const acrescimo_desconto = new AcrescimoDescontoModel({ tipo: ind_acres_desc, valor: valor_acres_desc });
 
-    const venda_cancelada = lines[1].substr(19, 1) === 'S';
-
-    const linha_5 = lines.find(x => x.startsWith('5'));
+    const venda_cancelada = linha_1.substr(19, 1) === 'S';
 
     const quantidade_total_de_produtos = parseFixedPoint(linha_5.substr(7, 12), 2);
     const somatorio_valor_unitario = parseFixedPoint(linha_5.substr(21, 14), 2);
     const somatorio_valor_subtotal_item = parseFixedPoint(linha_5.substr(35, 14), 2);
     const somatorio_valor_pagamento = parseFixedPoint(linha_5.substr(49, 14), 2);
 
-    const cliente_tipo = lines[1].substr(62, 1).trim() || null;
-    const cliente_cpf = cliente_tipo === 'J' ? null : (lines[2].substr(1, 14).trim() || null);
-    const cliente_cnpj = cliente_tipo !== 'J' ? null : (lines[2].substr(1, 14).trim() || null);
-    const cliente_email = lines[2].substr(498, 50).trim() || null;
-    const cliente_nome = lines[2].substr(15, 100).trim() || null;
-    const cliente_telefone = lines[2].substr(548, 20).trim() || null;
-    const cliente_sexo = lines[2].substr(970, 1).trim() || null;
-    const cliente_observacao = lines[2].substr(675, 40).trim() || lines[1].substr(286, 55).trim() || null;
+    const cliente_tipo = linha_1.substr(62, 1).trim() || null;
+    const cliente_cpf = cliente_tipo === 'J' ? null : (linha_2.substr(1, 14).trim() || null);
+    const cliente_cnpj = cliente_tipo !== 'J' ? null : (linha_2.substr(1, 14).trim() || null);
+    const cliente_email = linha_2.substr(498, 50).trim() || null;
+    const cliente_nome = linha_2.substr(15, 100).trim() || null;
+    const cliente_telefone = linha_2.substr(548, 20).trim() || null;
+    const cliente_sexo = linha_2.substr(970, 1).trim() || null;
+    const cliente_observacao = linha_2.substr(675, 40).trim() || linha_1.substr(286, 55).trim() || null;
 
-    _tmp = lines[2].substr(659, 8).trim() || null;
+    _tmp = linha_2.substr(659, 8).trim() || null;
     const cliente_data_nascimento = !_tmp || parseInt(_tmp.substr(0, 4)) <= 1900 ? null : (_tmp.substr(0, 4) + '-' + _tmp.substr(4, 2) + '-' + _tmp.substr(6, 2));
-    _tmp = lines[2].substr(667, 8).trim() || null;
+    _tmp = linha_2.substr(667, 8).trim() || null;
     const cliente_data_cadastro = !_tmp || parseInt(_tmp.substr(0, 4)) <= 2000 ? null : (_tmp.substr(0, 4) + '-' + _tmp.substr(4, 2) + '-' + _tmp.substr(6, 2));
 
-    const cliente_endereco_logradouro = lines[2].substr(115, 255).trim() || null;
-    const cliente_endereco_bairro = lines[2].substr(420, 60).trim() || null;
-    const cliente_endereco_complemento = lines[2].substr(370, 50).trim() || null;
-    const cliente_endereco_numero = lines[2].substr(480, 9).trim() || null;
-    const cliente_endereco_cep = lines[2].substr(489, 8).trim() || null;
-    const cliente_endereco_municipio = lines[2].substr(568, 80).trim() || null;
-    const cliente_endereco_uf = lines[2].substr(648, 2).trim() || null;
-    const cliente_endereco_referencia = lines[2].substr(715, 50).trim() || null; //FIXME: confirmar comparando com o extrattor
+    const cliente_endereco_logradouro = linha_2.substr(115, 255).trim() || null;
+    const cliente_endereco_bairro = linha_2.substr(420, 60).trim() || null;
+    const cliente_endereco_complemento = linha_2.substr(370, 50).trim() || null;
+    const cliente_endereco_numero = linha_2.substr(480, 9).trim() || null;
+    const cliente_endereco_cep = linha_2.substr(489, 8).trim() || null;
+    const cliente_endereco_municipio = linha_2.substr(568, 80).trim() || null;
+    const cliente_endereco_uf = linha_2.substr(648, 2).trim() || null;
+    const cliente_endereco_referencia = linha_2.substr(715, 50).trim() || null; //FIXME: confirmar comparando com o extrattor
 
     const cliente_endereco = new EnderecoModel({ logradouro: cliente_endereco_logradouro, numero: cliente_endereco_numero, complemento: cliente_endereco_complemento, bairro: cliente_endereco_bairro, cep: cliente_endereco_cep, municipio: cliente_endereco_municipio, uf: cliente_endereco_uf, referencia: cliente_endereco_referencia });
 
     let itens = [];
     let item_found = false;
-    for(let i = 3; i < lines.length; i++){
+    for(let i = 0; i < lines.length; i++){
         if(!lines[i].startsWith('3A')){
             if(!item_found)
                 continue;
-            break;
+            if(lines[i].startsWith('3B') || lines[i].startsWith('5') || lines[i].startsWith('9')){
+                break;
+            }
+            else {
+                itens[itens.length - 1].observacao = [itens[itens.length - 1].observacao || '', lines[i]].join('\n').trim();
+                continue;
+            }
         }
         item_found = true;
 
